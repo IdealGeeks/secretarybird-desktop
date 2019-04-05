dcd272db0ee93382060d67014e59f3976f81363d
Helike Long <helikelong@gmail.com>
2019-04-05 00:00:17 -0300

    Implements new theme

--- resources/views/admin/elements/menu.blade.php
+++ resources/views/admin/elements/menu.blade.php
@@ -1,6 +1,6 @@
-<ul class="navigation-menu">
+<ul class="nav navigation-menu">
     <li>
-        <a class="link-out" href="{{route('admin.dashboard')}}"><i class="mdi mdi-home"></i>Dashboard</a>
+        <a class="link-out" href="{{route('admin.dashboard')}}"><i class="mdi mdi-home"></i><p>Dashboard</p></a>
     </li>
     @isset($arrMenu)
         @foreach($arrMenu as $menu)
@@ -9,14 +9,14 @@
             @endif
             <li class="{{ count($menu['childs']) ? 'has-submenu' : '' }}">
                 <a class="{{ count($menu['childs']) ? '' : 'link-out' }} {{ $menu['rota'] ? '' : 'disable' }}" href="{{ (!count($menu['childs']) && $menu['rota']) ? route($menu['rota']) : '#' }}">
-                    <i class="{{ $menu['icone'] ?: '' }}"></i>{{ $menu['nome'] }}
+                    <i class="{{ $menu['icone'] ?: '' }}"></i><p>{{ $menu['nome'] }}</p>
                 </a>
                 @if(count($menu['childs']))
                     <ul class="submenu">
                         <li>
                             <ul>
                                 @foreach($menu['childs'] as $submenu)
-                                    <li><a class="link-out {{ $submenu['rota'] ? '' : 'disable' }}" href="{{ $submenu['rota'] ? route($submenu['rota']) : '#' }}">{{ $submenu['nome'] }}</a></li>
+                                    <li><a class="link-out {{ $submenu['rota'] ? '' : 'disable' }}" href="{{ $submenu['rota'] ? route($submenu['rota']) : '#' }}"><p>{{ $submenu['nome'] }}</p></a></li>
                                 @endforeach
                             </ul>
                         </li>
